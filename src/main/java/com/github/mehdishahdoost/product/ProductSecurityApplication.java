a0dc4250bebe9ae53fe2c768d783fddb49fd5e54
Mehdi Shahdoost <mhdshahdoost@gmail.com>
2021-06-25 12:33:18 +0430

    init

---
+++ src/main/java/com/github/mehdishahdoost/product/ProductSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.mehdishahdoost.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mehdishahdoost/product/ProductSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mehdishahdoost.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
